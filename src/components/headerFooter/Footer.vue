f8ed796c42f772b3d3335b2f644fab6011fb42f9
Adonat74 <donatgoninet.antoine@gmail.com>
2024-11-12 17:02:40 +0100

    started the header and the footer

--- src/components/headerFooter/Footer.vue
+++ src/components/headerFooter/Footer.vue
@@ -3,9 +3,47 @@
 </script>
 
 <template>
-<h3>Je suis le footer</h3>
+  <footer>
+    <div>
+      <section>
+        <h4>ÉCHAPPÉE BELLE</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.Vel quos deserunt sit laudantium neque ipsam minima facere.</p>
+      </section>
+      <section>
+        <h4>HORAIRES D'OUVERTURE</h4>
+        <p>Lundi - Samedi : 09:00 - 17:00 <br>Dimanche et jours fériés: Fermé</p>
+      </section>
+      <section>
+        <h4>NEWSLETTER</h4>
+        <div>
+          <input type="email">
+          <button><img src="@/assets/logo-réseaux-sociaux/boutton-submit-footer.svg" alt="icon newsletter"></button>
+        </div>
+        <p>Inscrivez-vous pour recevoir les
+          dernières offres</p>
+      </section>
+      <section>
+        <h4>RESEAUX SOCIAUX</h4>
+        <div>
+          <img src="@/assets/logo-réseaux-sociaux/facebook-logo-carré.svg" alt=""><img src="@/assets/logo-réseaux-sociaux/twitter-logo-carré.svg" alt=""><img src="@/assets/logo-réseaux-sociaux/instagram.svg" alt=""><img src="@/assets/logo-réseaux-sociaux/google-plus-g.svg" alt=""><img src="@/assets/logo-réseaux-sociaux/youtube.svg" alt="">
+        </div>
+      </section>
+    </div>
+    <p class="copyright">Copyright © Club Auto - Tous droits réservés</p>
+
+
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  background-color: var(--black);
+  color: white;
+  height: 280px;
+  margin-top: 100px;
+  top: calc(100vh - 280px);
+}
+.copyright{
+  color: black;
+}
 </style>
--- src/components/headerFooter/Header.vue
+++ src/components/headerFooter/Header.vue
@@ -4,18 +4,110 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <header>
-    <h3>Je suis le header</h3>
-    <nav>
-      <RouterLink to="/">Accueil</RouterLink>
-      <RouterLink to="/renting">Location</RouterLink>
-      <RouterLink to="/account/1">Compte</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
-    </nav>
+    <div class="headerContainer">
+      <p class="title">ÉCHAPPÉE BELLE</p>
+      <div class="flexRow">
+        <img src="@/assets/images/icone-adresse.png" alt="pin point adress icon">
+        <p><span style="color: var(--orange)">ADRESSE : </span><br>AVENUE CHARLES DE GAULLE <br>75000 PARIS</p>
+      </div>
+      <div class="flexRow">
+        <img src="@/assets/images/icon-telephone.png" alt="telephone icon">
+        <p><span style="color: var(--orange)">TÉLÉPHONE :</span><br>01 23 45 67 89</p>
+      </div>
+      <div>
+        <p><span style="color: var(--orange)">OUVERTURE :</span> Du Lundi au Samedi</p>
+        <p><span style="color: var(--orange)">HORAIRES :</span> De 9h à 17h</p>
+      </div>
+      <div class="socialIcons">
+        <img src="@/assets/logo-réseaux-sociaux/facebook-logo.svg" alt="facebook logo">
+        <img src="@/assets/logo-réseaux-sociaux/twitter-logo.svg" alt="twitter logo">
+      </div>
+    </div>
+
+
+    <div class="navContainer">
+      <nav>
+        <RouterLink to="/" :class="{ 'active-link': $route.path === '/' }">ACCUEIL</RouterLink>
+        <RouterLink to="/renting" :class="{ 'active-link': $route.path === '/renting' }">LOCATION</RouterLink>
+        <RouterLink to="/account" :class="{ 'active-link': $route.path === '/account' }">COMPTE</RouterLink>
+        <RouterLink to="/contact" :class="{ 'active-link': $route.path === '/contact' }">CONTACT</RouterLink>
+      </nav>
+    </div>
+
   </header>
 
 
 </template>
 
 <style scoped>
+header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+}
+
+.headerContainer {
+  background-color: var(--black);
+  color: white;
+  height: 100px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 0.8rem;
+  font-weight: 600;
+}
+.title{
+  font-weight: bold;
+  font-size: 2rem;
+}
+img{
+  height: 40px
+}
+
+.flexRow {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.flexRow > p {
+  margin-left: 5px;
+}
+.socialIcons {
+  display: flex;
+  align-items: center;
+  width: 100px;
+  justify-content: space-between;
+}
+
+.navContainer{
+  width: 100%;
+  height: 55px;
+  background-color: var(--tGray);
+}
+
+nav{
+  width: 100%;
+  height: 100%;
+  margin-left: 60%;
+  display: flex;
+  align-items: center;
+}
+
+nav a{
+  height: 100%;
+  padding: 0 30px;
+  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+}
+
+nav a.active-link {
+  background-color:  var(--orange);
+  color: #fff;
+}
+
 
 </style>
